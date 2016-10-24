1a346a510074dbffc5666b5a3fb9186b7788a386
Oftatkofta <oftakofta@gotmail.com>
2016-10-24 20:57:59 +0200

    FPS test on pi

--- modules/FPS_test.py
+++ modules/FPS_test.py
@@ -11,8 +11,12 @@ class FPS(Module):
 		self.screen.clear_pixel()
 		for x in range(16):
 			for y in range(16):
-				self.screen.pixel[x][y] = Color(255,255,255)
-		self.screen.update()
-	
+				self.screen.pixel[x][y] = Color(0,255,255)
+				self.screen.update()
+				#time.sleep(0.1)
+		#self.screen.update()
+
+
 	def tick(self):
 		self.draw()
+		#time.sleep(0.1)
