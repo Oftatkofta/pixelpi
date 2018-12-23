a77d7ec45f1de80fde884a388450e25b5c33aa53
Jens Eriksson <jenseri@gmail.com>
2018-12-23 20:19:32 +0100

    get_colorlist method added to screen

--- screen/__init__.py
+++ screen/__init__.py
@@ -1,4 +1,4 @@
-import helpers
+from helpers import Color, RGBColor, int_to_rgb_color, rgb_tuple_to_int, darken_color
 import time
 from PIL import Image
 
@@ -8,13 +8,13 @@ class AbstractScreen(object):
         self.height = height
 
         #Pixels to shift out to the strip stored as int
-        self.pixel = [[helpers.Color(0,0,0) for y in range(height)] for x in range(width)]
+        self.pixel = [[Color(0,0,0) for y in range(height)] for x in range(width)]
 
         #A PIL-Image object to use for complex image manipulations
         self.stage = Image.new("RGB", (width, height))
 
 
-    def clear_pixel(self, color = helpers.Color(0, 0, 0)):
+    def clear_pixel(self, color = Color(0, 0, 0)):
         for x in range(self.width):
             for y in range(self.height):
                 self.pixel[x][y] = color
@@ -26,7 +26,7 @@ class AbstractScreen(object):
         #moves the pixels from the staging image to self.pixel
         for x in range(self.width):
             for y in range(self.height):
-                px = helpers.rgb_tuple_to_int(self.stage.getpixel((x,y)))
+                px = rgb_tuple_to_int(self.stage.getpixel((x,y)))
                 self.pixel[x][y] = px
 
     def update(self):
@@ -42,7 +42,7 @@ class AbstractScreen(object):
             progress = (time.time() - start) / duration
             if not fadein:
                 progress = 1.0 - progress
-            self.pixel = [[helpers.darken_color(frame[x][y], progress) for y in range(self.height)] for x in range(self.width)]
+            self.pixel = [[darken_color(frame[x][y], progress) for y in range(self.height)] for x in range(self.width)]
             self.update()
 
     def fade_in(self, duration=0.5):
@@ -50,3 +50,20 @@ class AbstractScreen(object):
 
     def fade_out(self, duration=0.5):
         self.fade(duration, False)
+
+    def get_colorlist(self):
+        """
+        Returns a list of the unique RGB colors in the currently displayed image
+
+        :return: (list) unique Color objects in self.pixel
+        """
+
+        out = []
+        for x in range(self.width):
+            for y in range(self.height):
+                pixel = self.pixel[x][y]
+
+                if pixel not in out:
+                    out.append(pixel)
+
+        return out
