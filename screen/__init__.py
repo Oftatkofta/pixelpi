31d98db6916c8cab008f0956d6bea672b0f47809
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2024-12-07 00:34:18 +0100

    documentation

--- screen/__init__.py
+++ screen/__init__.py
@@ -3,7 +3,18 @@ import time
 from PIL import Image
 
 class AbstractScreen(object):
+    """
+    Abstract base class for a screen or display, providing a framework for
+    managing and manipulating a matrix of pixels and a staging image for complex
+    image operations.
+    """
     def __init__(self, width = 16, height = 16):
+        """
+        Initializes the screen with the given dimensions.
+        
+        :param width: The width of the screen in pixels.
+        :param height: The height of the screen in pixels.
+        """
         self.width = width
         self.height = height
 
@@ -15,14 +26,26 @@ class AbstractScreen(object):
 
 
     def clear_pixel(self, color = Color(0, 0, 0)):
+        """
+        Clears the pixel matrix by setting all pixels to the specified color.
+        
+        :param color: The color to set all pixels to (default is black).
+        """
         for x in range(self.width):
             for y in range(self.height):
                 self.pixel[x][y] = color
 
     def clear_stage(self):
+        """
+        Clears the staging image by creating a new image with the screen's dimensions.
+        """
         self.stage = Image.new("RGB", (self.width, self.height))
 
     def stage_to_pixels(self):
+        """
+        Transfers the staged image to the pixel matrix by converting each pixel of the
+        image to an integer representation and updating the pixel matrix.
+        """
         #moves the pixels from the staging image to self.pixel
         for x in range(self.width):
             for y in range(self.height):
@@ -30,9 +53,18 @@ class AbstractScreen(object):
                 self.pixel[x][y] = px
 
     def update(self):
+        """
+        Placeholder method for updating the display. This should be implemented by Screen subclasses.
+        """
         pass
 
     def fade(self, duration, fadein):
+        """
+        Performs a fade in or fade out animation over the specified duration.
+        
+        :param duration: The duration of the fade effect in seconds.
+        :param fadein: A boolean indicating whether to fade in (True) or out (False).
+        """
         frame = [[self.pixel[x][y] for y in range(self.height)] for x in range(self.width)]
 
         start = time.time()
@@ -46,18 +78,27 @@ class AbstractScreen(object):
             self.update()
 
     def fade_in(self, duration=0.5):
+        """
+        Initiates a fade-in effect from black to the current image over the given duration.
+        
+        :param duration: The duration of the fade-in effect in seconds (default is 0.5).
+        """
         self.fade(duration, True)
 
     def fade_out(self, duration=0.5):
+        """
+        Initiates a fade-out effect from the current image to black over the given duration.
+        
+        :param duration: The duration of the fade-out effect in seconds (default is 0.5).
+        """
         self.fade(duration, False)
 
     def get_colorlist(self):
         """
-        Returns a list of the unique RGB colors in the currently displayed image
-
-        :return: (list) unique Color objects in self.pixel
+        Retrieves a list of unique RGB colors currently displayed on the screen.
+        
+        :return: A list of unique Color objects in the pixel matrix.
         """
-
         out = []
         for x in range(self.width):
             for y in range(self.height):
--- screen/virtualscreen.py
+++ screen/virtualscreen.py
@@ -2,9 +2,28 @@ import pygame
 import helpers
 from screen import AbstractScreen
 
-# Behaves like the actual LED screen, but shows the screen content on a computer screen
 class VirtualScreen(AbstractScreen):
-	def __init__(self, width = 16, height = 16, led_pin = 18, led_freq_hz = 800000, led_dma = 5, led_invert = False, led_brightness = 200):		
+	"""
+	VirtualScreen simulates an LED screen using Pygame to render the display on a computer screen.
+	
+	Attributes:
+		pixel_size (int): Size of each 'pixel' on the virtual screen.
+		screen (pygame.Surface): The Pygame window surface where the virtual screen is displayed.
+		surface (pygame.Surface): The Pygame surface that is used to draw the virtual screen content.
+	"""
+	def __init__(self, width=16, height=16, led_pin=18, led_freq_hz=800000, led_dma=5, led_invert=False, led_brightness=200):
+		"""
+		Initializes a new VirtualScreen instance with the given dimensions and LED parameters.
+		
+		Args:
+			width (int): The width of the virtual screen in pixels. Defaults to 16.
+			height (int): The height of the virtual screen in pixels. Defaults to 16.
+			led_pin (int): The GPIO pin connected to the LED strip. Defaults to 18.
+			led_freq_hz (int): The frequency of the LED signal in hertz. Defaults to 800000.
+			led_dma (int): The DMA channel used for generating the PWM signal. Defaults to 5.
+			led_invert (bool): Whether to invert the LED signal. Defaults to False.
+			led_brightness (int): The brightness of the LEDs. Defaults to 200.
+		"""
 		super(VirtualScreen, self).__init__(width, height)
 		self.pixel_size = 30
 		
@@ -13,24 +32,14 @@ class VirtualScreen(AbstractScreen):
 												height * self.pixel_size),
 												pygame.RESIZABLE)
 
-		self.surface = pygame.Surface(self.screen.get_size())	
+		self.surface = pygame.Surface(self.screen.get_size())    
 
-def update(self):
-    """
-    Updates the virtual screen by drawing the LED matrix to the Pygame surface and displaying it.
-    This method converts the internal GRB color format to RGB for proper display.
-    """
-    for event in pygame.event.get():
-        if event.type == pygame.VIDEORESIZE:
-            # Update the screen size
-            self.screen = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
-            self.surface = pygame.Surface(self.screen.get_size())
-    
-    for y in range(self.height):
-        for x in range(self.width):
-            # Convert GRB color format to RGB for display
-            adjusted_color = helpers.int_to_rgb_color(self.pixel[x][y])
-            pygame.draw.rect(self.surface, adjusted_color, ((x * self.pixel_size, y * self.pixel_size), (((x+1) * self.pixel_size), (y+1) * self.pixel_size)))
-
-    self.screen.blit(self.surface, (0, 0))
-    pygame.display.flip()  # flip() is enough since we're updating the whole screen
+	def update(self):
+		for y in range(self.height):
+			for x in range(self.width):
+				#colors are in GRB format on the LED strip, to display properly we need to convert to a RGB tuple
+				adjusted_color = helpers.int_to_rgb_color(self.pixel[x][y])
+				pygame.draw.rect(self.surface, adjusted_color, ((x * self.pixel_size, y * self.pixel_size), (((x+1) * self.pixel_size), (y+1) * self.pixel_size)))
+				self.screen.blit(self.surface, (0, 0))
+		pygame.display.flip()
+		pygame.display.update()
